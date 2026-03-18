ec7e4b87a3147dc00dcbad9c7a5a88832c322f26
Gourav More <gourav_m@tekditechnologies.com>
2026-03-18 12:01:02 +0530

    Issue #0000 fix: CSP issue fixes

--- src/csp.js
+++ src/csp.js
@@ -14,8 +14,8 @@ export const getCSP = (envVariables) => {
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.cdnfonts.com/;
       font-src 'self' https://fonts.gstatic.com https://fonts.cdnfonts.com;
       img-src 'self' data: https://raw.githubusercontent.com https://cdn.jsdelivr.net https://github.com https://images.squarespace-cdn.com ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} ${REACT_APP_AWS_S3_BUCKET_URL};
-      media-src 'self' blob: ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} https://raw.githubusercontent.com https://github.com ;
-      connect-src 'self' *.theall.ai ${REACT_APP_HOST} ${REACT_APP_LEARNER_AI_BASE_URL} ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} blob: https://huggingface.co https://cas-bridge.xethub.hf.co https://cdn.jsdelivr.net https://all-saas-keycloak.tekdinext.com https://all-saas-keycloak.tekdinext.com/all-orchestration-services https://all-saas-keycloak.tekdinext.com/telemetry-service https://all-saas-keycloak.tekdinext.com/lais ;
+      media-src 'self' blob: ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} https://allsaas-useraudios.s3.ap-south-1.amazonaws.com https://raw.githubusercontent.com https://github.com ;
+      connect-src 'self' *.theall.ai ${REACT_APP_HOST} ${REACT_APP_LEARNER_AI_BASE_URL} ${REACT_APP_AWS_S3_BUCKET_URL} ${REACT_APP_AWS_S3_BUCKET_CONTENT_URL} https://allsaas-useraudios.s3.ap-south-1.amazonaws.com blob: https://huggingface.co https://cas-bridge.xethub.hf.co https://cdn.jsdelivr.net https://all-saas-keycloak.tekdinext.com https://all-saas-keycloak.tekdinext.com/all-orchestration-services https://all-saas-keycloak.tekdinext.com/telemetry-service https://all-saas-keycloak.tekdinext.com/lais ;
       form-action 'self';
       frame-src 'self' https://www.google.com https://www.gstatic.com https://www.youtube.com https://www.youtube-nocookie.com;
       object-src 'none';
